a204e7d14b6e942fcd0660ae7bd47d418be1b8be
vakakvekveskiri <vakakvekvesqiri@gmail.com>
2024-12-05 11:59:00 +0400

    Delete davaleba_7.py

--- davaleba_7.py
+++
@@ -1,9 +0,0 @@
-def generate_pattern(n):
-
-
-    for i in range(1, n + 1):
-
-        print(''.join(str(x) for x in range(1, i + 1)))
-
-number = int(input("chawere raime: "))
-generate_pattern(number)
